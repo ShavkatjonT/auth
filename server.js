08887d4b01c7aa61cecbcb63a836393ab5361bc4
ShavkatjonT <105767759+ShavkatjonT@users.noreply.github.com>
2024-01-12 13:08:03 +0500

    db error fixed

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header({
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-      "Access-Control-Allow-Headers": "*",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    "Access-Control-Allow-Headers": "*",
   });
   next();
 });
@@ -26,10 +26,7 @@ const Role = db.role;
 
 // db.sequelize.sync();
 // force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
+
 
 // simple route
 app.get("/", (req, res) => {
@@ -50,12 +47,12 @@ function initial() {
     id: 1,
     name: "user"
   });
- 
+
   Role.create({
     id: 2,
     name: "moderator"
   });
- 
+
   Role.create({
     id: 3,
     name: "admin"
@@ -65,7 +62,9 @@ function initial() {
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, async () => {
   await db.sequelize.authenticate();
-  await db.sequelize.sync();
-
+  await db.sequelize.sync({ force: false }).then(() => {
+    console.log('Drop and Resync Database with { force: true }');
+    initial();
+  });
   console.log(`Server is running on port ${PORT}.`);
 });
